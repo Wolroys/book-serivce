04e9136968b9e64501a593f5188ac6bf7f361a11
Aleksey Paslenov <alekseypaslenov@gmail.com>
2024-01-28 18:08:50 +0300

    added config and server

---
+++ booking-config/src/main/java/org/sber/bookingconfig/BookingConfigApplication.java
@@ -0,0 +1,13 @@
+package org.sber.bookingconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingConfigApplication.class, args);
+    }
+
+}
---
+++ booking-config/src/test/java/org/sber/bookingconfig/BookingConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sber.bookingconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
